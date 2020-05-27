7d2fd07de6d61f624f4ecebc005e06a4527e5d8e
Kasper Flensburg Petersen <31951412+kasp4770@users.noreply.github.com>
2020-05-27 12:48:10 +0200

    CORS fix , maybe

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const helmet = require('helmet');
 const port = 3000
 const DBdevConfig = require('./config/database').development;
 const Sequelize = require('sequelize'); 
-//router imports
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const indexRouter = require('./routes/indexRouter');
 
 const sequelize = new Sequelize(DBdevConfig.database, DBdevConfig.username, DBdevConfig.password, {
@@ -37,7 +39,20 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 
-app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+//app.use(helmet());
+app.use(cors({
+  origin: '*',
+  optionsSuccessStatus: 200
+}));
+/*app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})*/
+
+
 app.use('/api/restaurants', indexRouter); //Restaurants List
 
  
