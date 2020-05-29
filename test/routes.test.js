2b5e9cd106a451a43e66162e033c068360626a80
Kasper Flensburg Petersen <31951412+kasp4770@users.noreply.github.com>
2020-05-29 15:37:19 +0200

    Router tests done

--- test/routes.test.js
+++ test/routes.test.js
@@ -1,66 +1,26 @@
+//Test data import
+const testData = require('./testData');
+
 const app = require('../server');
 const chai = require('chai');
 const sinon = require('sinon');
 const Restaurant = require('../dal/restaurantDAO');
 const request = require('supertest');
 const expect = chai.expect;
-const assert = chai.assert;
-//const chaiHttp = require('chai-http');
-//chai.use(chaiHttp);
 
-//var requester = chai.request(app);
+/**
+ * Making stubs for each test to avoid connecting to db to get data, thereby the tests can run faster. 
+ * Also making sure the stubs get called, therfore the real method in the real application will also get called on.
+ * The corresponding real db method is tested in restaurantDAO.test.js.
+ */
 
-describe('Some name', function () {
+describe('indexRouter tests', function () {
     context('Route GET \'/api/restaurants\'', function () {
+        //Data for stub to return
+        const fakeData = testData.testRestaurants;
 
-        it('indexController index()', async function () {
-            const fakeData = [
-                {
-                  id: 1,
-                  name: 'Test1',
-                  logoRelativePath: '/restaurantLogos/macro',
-                  openAtHour: 1200,
-                  closedAtHour: 2000,
-                  deliveryPrice: 20,
-                  deliveryLowerBoundary: 100,
-                  deliveryUpperBoundary: 250,
-                  deliveryTimeMinutes: 30,
-                  rating: 4.9,
-                  address: 'Jernbanegade 16',
-                  postalCode: 4700,
-                  city: 'Næstved'
-                },
-                {
-                  id: 2,
-                  name: 'Test2',
-                  logoRelativePath: '/restaurantLogos/dhaba',
-                  openAtHour: 1200,
-                  closedAtHour: 2200,
-                  deliveryPrice: 10,
-                  deliveryLowerBoundary: 0,
-                  deliveryUpperBoundary: null,
-                  deliveryTimeMinutes: 30,
-                  rating: 4.2,
-                  address: 'Sct. Peders Kirkeplads 1',
-                  postalCode: 4700,
-                  city: 'Næstved'
-                },
-                {
-                  id: 3,
-                  name: 'Test3',
-                  logoRelativePath: '/restaurantLogos/cafeelysia',
-                  openAtHour: 1600,
-                  closedAtHour: 2200,
-                  deliveryPrice: 0,
-                  deliveryLowerBoundary: 50,
-                  deliveryUpperBoundary: 150,
-                  deliveryTimeMinutes: 45,
-                  rating: 4.6,
-                  address: 'Jernbanegade 15',
-                  postalCode: 4700,
-                  city: 'Næstved'
-                }
-            ];
+        it('index middleware function respond 200, json, and calls getAll from DAO', async function () {
+            //Stub
             const restaurant = Restaurant;
             const stub = sinon
                 .stub(restaurant, 'getAll')
@@ -70,39 +30,86 @@ describe('Some name', function () {
                 .expect(200)
                 .expect('Content-Type', /json/)
                 .expect(fakeData);
-
             
-            //expect(stub.getCall(0).args[0]).to.equal('Test1');
-
+            //Test fails if stub is not called
+            expect(stub.called).to.be.true;
+            
+            //Restore behavior of method. Else it would disturb other tests in other testfiles.
             stub.restore();
         })
     })
 
-    xcontext('Route GET \'api/restaurant/:id\'', async function () {
+    context('Route GET \'api/restaurant/:id\'', async function () {
+        //Data for stub to return
+        const fakeData = testData.testSingleRestaurant;
 
-        //let res = await requester.get('/api/restaurants/3')
+        it('getById middleware function respond 200, json, and calls getById from DAO', async function () {
+            //Stub
+            const restaurant = Restaurant;
+            const stub = sinon
+                .stub(restaurant, 'getById')
+                .resolves(fakeData);
 
-        it('status 200', () => {
-            let actual = res.status
-            let expected = 200;
+            await request(app).get('/api/restaurants/2')
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .expect(fakeData);
 
-            assert.equal(actual, expected);
-        })
+            //Test fails if stub is not called
+            expect(stub.called).to.be.true;
 
-        it('content-Type equals application/json; charset-utf-8', () => {
-            let actual = res.header['content-type'];
-            let expected = 'application/json; charset=utf-8'
+            //Restore behavior of method. Else it would disturb other tests in other testfiles.
+            stub.restore();
         })
     })
 
-    xcontext('Route POST \'/:id/order\'', () => {
+    context('Route GET \'api/restaurants/:id/order/:id2\'', () => {
 
-        it('User sends his/her information', () => {
-            let actual = 1 + 2;
-            let expected = 3;
+        //Data for stub to return
+        const fakeData = testData.testOrder;
 
-            assert.equal(actual, expected);
+        it('getOrderById middleware function respond 200, json, and calls getOrderById from DAO', async function () {
+            //Stub
+            const restaurant = Restaurant;
+            const stub = sinon
+                .stub(restaurant, 'getOrderById')
+                .resolves(fakeData);
+
+            await request(app).get('/api/restaurants/2/order/1')
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .expect(fakeData);
+
+            //Test fails if stub is not called
+            expect(stub.called).to.be.true;
+
+            //Restore behavior of method. Else it would disturb other tests in other testfiles.
+            stub.restore();
         })
+    })
 
+    context('Route POST \'api/restaurants/:id/order\'', () => {
+
+        //Data for stub to return
+        const fakeData = testData.testOrder;
+
+        it('createOrder middleware function respond 201, json, and calls addOrder from DAO', async function () {
+            //Stub
+            const restaurant = Restaurant;
+            const stub = sinon
+                .stub(restaurant, 'addOrder')
+                .resolves(fakeData);
+
+            await request(app).post('/api/restaurants/3/order')
+                .expect(201)
+                .expect('Content-Type', /json/)
+                .expect(fakeData);
+
+            //Test fails if stub is not called
+            expect(stub.called).to.be.true;
+
+            //Restore behavior of method. Else it would disturb other tests in other testfiles. 
+            stub.restore();
+        })
     })
 })
